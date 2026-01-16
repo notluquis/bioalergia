5e65b66443d0c9c643f7e02bf6a81aabb1b48b67
Lucas Pulgar-Escobar <luucasmatiaspulgar@outlook.cl>
2026-01-15 23:22:19 -0300

    fix: restore lint suppression to unblock CI

--- apps/web/src/lib/authz/AbilityProvider.tsx
+++ apps/web/src/lib/authz/AbilityProvider.tsx
@@ -1,12 +1,12 @@
 // src/lib/authz/AbilityProvider.tsx
-/* eslint-disable sonarjs/deprecation */
-import { Ability } from "@casl/ability";
+
+import { MongoAbility } from "@casl/ability";
 import { createContextualCan } from "@casl/react";
 import React, { createContext } from "react";
 
 import { ability } from "./ability";
 
-export const AbilityContext = createContext<Ability>(ability);
+export const AbilityContext = createContext<MongoAbility>(ability);
 export const Can = createContextualCan(AbilityContext.Consumer);
 
 export const AbilityProvider = ({ children }: { children: React.ReactNode }) => {
