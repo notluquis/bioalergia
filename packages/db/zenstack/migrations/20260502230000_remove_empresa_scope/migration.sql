3104801e51b538536d5d1f01a69db1c99a3e8666
Lucas Pulgar-Escobar <luucasmatiaspulgar@outlook.cl>
2026-05-02 13:11:42 -0400

    fix(migration): drop column default before recreating ExpenseScope enum

--- packages/db/zenstack/migrations/20260502230000_remove_empresa_scope/migration.sql
+++ packages/db/zenstack/migrations/20260502230000_remove_empresa_scope/migration.sql
@@ -3,6 +3,9 @@ UPDATE expenses SET scope = 'BIOALERGIA' WHERE scope = 'EMPRESA';
 UPDATE expense_services SET scope = 'BIOALERGIA' WHERE scope = 'EMPRESA';
 UPDATE "personal".utility_accounts SET scope = 'BIOALERGIA' WHERE scope = 'EMPRESA';
 
+-- Drop column defaults that reference the old enum type before recreating it
+ALTER TABLE "personal".utility_accounts ALTER COLUMN scope DROP DEFAULT;
+
 -- Postgres doesn't support DROP VALUE on enums, so we recreate it
 ALTER TYPE "ExpenseScope" RENAME TO "ExpenseScope_old";
 CREATE TYPE "ExpenseScope" AS ENUM ('BIOALERGIA', 'PERSONAL');
@@ -13,5 +16,8 @@ ALTER TABLE expense_services
   ALTER COLUMN scope TYPE "ExpenseScope" USING scope::text::"ExpenseScope";
 ALTER TABLE "personal".utility_accounts
   ALTER COLUMN scope TYPE "ExpenseScope" USING scope::text::"ExpenseScope";
+
+-- Re-add default using new type
+ALTER TABLE "personal".utility_accounts ALTER COLUMN scope SET DEFAULT 'PERSONAL';
 
 DROP TYPE "ExpenseScope_old";
